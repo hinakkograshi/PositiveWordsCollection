7cd54af18d3135d28b83ff36898edeecc1882c40
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-05-18 00:10:05 +0900

    add: SwiftLint

---
+++ PositiveWordsCollection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PositiveWordsCollection
+//
+//  Created by Hina on 2024/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PositiveWordsCollection/PositiveWordsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositiveWordsCollectionApp.swift
+//  PositiveWordsCollection
+//
+//  Created by Hina on 2024/05/17.
+//
+
+import SwiftUI
+
+@main
+struct PositiveWordsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
