fe0505dd7cdc1f7f00206bb91cb9444adefc28f5
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-06-08 19:28:18 +0900

    add:投稿数・いいね数反映

--- PositiveWordsCollection/View/Profile/ProfileHeaderView.swift
+++ PositiveWordsCollection/View/Profile/ProfileHeaderView.swift
@@ -12,32 +12,35 @@ struct ProfileHeaderView: View {
     @Binding var profileImage: UIImage
     @Binding var profileBio: String
     @ObservedObject var postArray: PostArrayObject
+    
     var body: some View {
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+        VStack(alignment: .center, spacing: 10, content: {
             // MARK: PROFILE PICTURE
             HStack(alignment: .center, spacing: 20, content: {
                 Image(uiImage: profileImage)
                     .resizable()
                     .scaledToFill()
-                    .frame(width: 100, height: 100, alignment: .leading)
+                    .frame(width: 80, height: 80, alignment: .leading)
                     .clipShape(RoundedRectangle(cornerRadius: 60))
                 // MARK: USER NAME
                 Text(profileDisplayName)
-                    .font(.largeTitle)
+                    .font(.title)
                     .fontWeight(.bold)
             })
 
             // MARK: BIO
-            Text(profileBio)
-                .font(.title3)
-                .fontWeight(.regular)
-                .multilineTextAlignment(.center)
-            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+            if profileBio != "" {
+                Text(profileBio)
+                    .font(.title3)
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.center)
+            }
+            HStack(alignment: .center, spacing: 50, content: {
                 // MARK: POSTS
                 VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, content: {
                     HStack {
                         Image(systemName: "paperplane")
-                        Text("20")
+                        Text(postArray.postCountString)
                             .font(.title2)
                             .fontWeight(.bold)
                     }
@@ -55,7 +58,7 @@ struct ProfileHeaderView: View {
                     HStack {
                         Image(systemName: "heart.fill")
                             .foregroundStyle(.red)
-                        Text("100")
+                        Text(postArray.likeCountString)
                             .font(.title2)
                             .fontWeight(.bold)
                     }
@@ -70,7 +73,6 @@ struct ProfileHeaderView: View {
                 })
             })
         })
-        .padding()
     }
 }
 
