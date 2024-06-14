700caf35ac82aac28dc027c16f78d597272e0614
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-06-15 00:24:37 +0900

    add: CreatePostViewで画面タップでキーボードを閉じる

--- PositiveWordsCollection/View/Profile/ProfileView.swift
+++ PositiveWordsCollection/View/Profile/ProfileView.swift
@@ -31,6 +31,7 @@ struct ProfileView: View {
 
     var body: some View {
         ProfileHeaderView(profileDisplayName: $profileDisplayName, profileImage: $profileImage, profileBio: $profileBio, postArray: posts)
+            .padding(.top, 10)
         Rectangle()
             .foregroundStyle(.orange)
             .frame(height: 2)
