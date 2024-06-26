e942e2d98ffa9381c68e5dbc74087c59991571b8
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-06-26 20:49:36 +0900

    add: DatabaseHelperField作成

---
+++ PositiveWordsCollection/Helper/DatabaseHelperField.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseHelperField.swift
+//  PositiveWordsCollection
+//
+//  Created by Hina on 2024/06/26.
+//
+
+import Foundation
+
+struct DatabaseHelperField {
+    static let userID = "user_id"
+    static let displayName = "display_name"
+    static let bio = "bio"
+    static let dateCreated = "date_created"
+    static let providerID = "provider_id"
+    static let likedBy = "liked_by"
+}
