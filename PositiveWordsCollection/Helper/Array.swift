1e1e397561e8e7142807bec9792d4fa5891b5a93
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-07-06 23:26:30 +0900

    add: 投稿非表示機能

---
+++ PositiveWordsCollection/Helper/Array.swift
@@ -0,0 +1,28 @@
+//
+//  Array.swift
+//  PositiveWordsCollection
+//
+//  Created by Hina on 2024/07/06.
+//
+
+import Foundation
+
+extension Array: RawRepresentable where Element: Codable {
+  public init?(rawValue: String) {
+    guard let data = rawValue.data(using: .utf8),
+          let result = try? JSONDecoder().decode([Element].self, from: data)
+    else {
+      return nil
+    }
+    self = result
+  }
+
+  public var rawValue: String {
+    guard let data = try? JSONEncoder().encode(self),
+          let result = String(data: data, encoding: .utf8)
+    else {
+      return "[]"
+    }
+    return result
+  }
+}
