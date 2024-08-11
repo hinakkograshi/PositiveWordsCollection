fc5350e0c8a2f0352d47ef9692f09786ff21ed91
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-08-11 22:32:32 +0900

    add: Xcode Cloud GoogleService-Info.plist環境変数に設定

---
+++ PositiveWordsCollection/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  PositiveWordsCollection
+#
+#  Created by Hina on 2024/08/11.
+#  
+echo $GOOGLE_SERVICE_INFO > ../PositiveWordsCollection/GoogleService-Info.plist
