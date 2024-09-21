61829139dc0c0c5a17a42e04bef0fbabbd7a9bd4
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-09-21 23:59:36 +0900

    update: structからclassに変更

--- PositiveWordsCollection/Model/PostModel.swift
+++ PositiveWordsCollection/Model/PostModel.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 
-struct PostModel: Hashable {
-    /*var id = UUID()*/ // identifiableに準拠させるためにすべてのポストモデルアイテムが固有のIDを設定
+class PostModel: Hashable {
     var postID: String // データベース内の投稿のID
     var userID: String // データベース内のユーザーのID
     var username: String // ユーザーとデータベースのユーザー名
@@ -17,6 +16,39 @@ struct PostModel: Hashable {
     var likeCount: Int // いいね数
     var likedByUser: Bool // ユーザーに気に入られているか
     var comentsCount: Int // 投稿数
+
+    init(postID: String, userID: String, username: String, caption: String, dateCreated: Date, likeCount: Int, likedByUser: Bool, comentsCount: Int) {
+        self.postID = postID
+        self.userID = userID
+        self.username = username
+        self.caption = caption
+        self.dateCreated = dateCreated
+        self.likeCount = likeCount
+        self.likedByUser = likedByUser
+        self.comentsCount = comentsCount
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(postID)
+        hasher.combine(userID)
+        hasher.combine(username)
+        hasher.combine(caption)
+        hasher.combine(dateCreated)
+        hasher.combine(likeCount)
+        hasher.combine(likedByUser)
+        hasher.combine(comentsCount)
+    }
+
+    static func == (lhs: PostModel, rhs: PostModel) -> Bool {
+        return lhs.postID == rhs.postID &&
+               lhs.userID == rhs.userID &&
+               lhs.username == rhs.username &&
+               lhs.caption == rhs.caption &&
+               lhs.dateCreated == rhs.dateCreated &&
+               lhs.likeCount == rhs.likeCount &&
+               lhs.likedByUser == rhs.likedByUser &&
+               lhs.comentsCount == rhs.comentsCount
+    }
 }
 
 extension PostModel: Identifiable {
