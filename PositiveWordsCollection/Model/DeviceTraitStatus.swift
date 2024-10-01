d02d92dcb18925c22cbe478f6e33c45bb4cf49a2
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-10-02 07:58:36 +0900

    add: DeviceTraitStatusのenum作成

---
+++ PositiveWordsCollection/Model/DeviceTraitStatus.swift
@@ -0,0 +1,31 @@
+//
+//  UserInterfaceSizeClass.swift
+//  PositiveWordsCollection
+//
+//  Created by Hina on 2024/10/02.
+//
+
+import SwiftUI
+
+enum DeviceTraitStatus {
+    case iPhoneHeight
+    case iPhoneWidth
+    case iPhoneLargeWidth
+    case iPad
+
+    init(hSizeClass: UserInterfaceSizeClass?, vSizeClass: UserInterfaceSizeClass?) {
+
+        switch (hSizeClass, vSizeClass) {
+        case (.compact, .regular):
+            self = .iPhoneHeight
+        case (.compact, .compact):
+            self = .iPhoneWidth
+        case (.regular, .compact):
+            self = .iPhoneLargeWidth
+        case (.regular, .regular):
+            self = .iPad
+        default:
+            self = .iPhoneHeight
+        }
+    }
+}
